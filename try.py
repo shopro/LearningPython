5eb06bedb8b853d16238bd1d7c0a671a5513a519
shopro <4585172+shopro@users.noreply.github.com>
2019-10-01 15:27:11 +0300

    Python 3 compatible

--- try.py
+++ try.py
@@ -7,29 +7,28 @@ for i in range(0,a):
 	#print z
 	ls.append(z)
 	i=i+1
-print ' start'
+print(' start')
 
 n=10
 def fn(n,z):
 	n=n+1
-	print n
-	print 'z:',z
+	print(n)
+	print('z:',z)
 	for j in range(0,len(ls),2):
-		print j
+		print(j)
 		ls[j]=ls[j]+1
 	#j=j+2
-		print j
+		print(j)
 	return j
 
-print ls
+print(ls)
 n=11
-a=fn(11)
-print 'a :',a
+a=fn(n, a)
+print('a :',a)
 #ls.insert(0,99)
 #j=0
 #length = len(ls)
 #length=length+5
 #print 'Length is : ',length
 
-print 'exit'
-
+print('exit')
